0f923eebd8627ded536d08e4b8e7a1b6561b473d
tonydreyes <68966023+tonydreyes@users.noreply.github.com>
2026-03-14 09:28:58 -0400

    Add contentManager.js with CRUD functions for articles, projects, contacts, and subscribers.

---
+++ contentManager.js
@@ -0,0 +1,21 @@
+// contentManager.js
+
+// Function to create new content
+function createContent(type, data) {
+    // Implementation for creating content
+}
+
+// Function to read content
+function readContent(type, id) {
+    // Implementation for reading content
+}
+
+// Function to update existing content
+function updateContent(type, id, data) {
+    // Implementation for updating content
+}
+
+// Function to delete content
+function deleteContent(type, id) {
+    // Implementation for deleting content
+}
